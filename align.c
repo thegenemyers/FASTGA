7547d050372bb36a9a9d63a4ec532f2bc6522b02
thegenemyers <gene.myers@gmail.com>
2024-01-11 19:24:52 +0100

    Update align.c

--- align.c
+++ align.c
@@ -173,7 +173,7 @@ void Free_Work_Data(Work_Data *ework)
 #define PATH_INT  0x0fffffffffffffffll   //  Must be PATH_TOP-1
 #define TRIM_MASK 0x7fff                 //  Must be (1 << TRIM_LEN) - 1
 #define TRIM_MLAG 250                    //  How far can last trim point be behind best point
-#define WAVE_LAG   30                    //  How far can worst point be behind the best point
+#define WAVE_LAG   60                    //  How far can worst point be behind the best point
 
 static double Bias_Factor[10] = { .690, .690, .690, .690, .780,
                                   .850, .900, .933, .966, 1.000 };
